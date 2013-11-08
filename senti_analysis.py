35604449aa70e8301774acc7b8c51561cfd40c09
Karthik Chandrasekar <karthikchandrasekar@Karthiks-MacBook-Pro.local>
2013-11-08 01:44:11 -0700

    Initial playing around with api's

--- senti_analysis.py
+++ senti_analysis.py
@@ -3,32 +3,40 @@ import nltk
 class sample_nltk:
     def __init__(self):
         self.samp_review = "No twist and turn. Just thala's presence made everything simple. BGM was classic. Aarambbam - a tribute to real heros, to and for thala fans. #bitch pls, it has just begun"        
-        self.pre_processed_words = []
-
 
     def run_main(self):
-        self.do_tokenize()
-        self.do_stemming()
+        tokenized_without_stop_words_list = self.do_tokenize()
+        self.do_stemming(tokenized_without_stop_words_list)
 
 
     def do_tokenize(self):
-            tokenized_words = nltk.word_tokenize(self.samp_review)
-            tokenized_words_set = set(tokenized_words)
-        
 
-            stop_words_set = set(nltk.corpus.stopwords.words())
-        
+        #Local ds
+        tokenized_without_stop_words_list = []
 
-            for word in tokenized_words:
-                if word not in stop_words_set:
-                    self.pre_processed_words.append(word)
-     
-            print tokenized_words
-            print self.pre_processed_words
+        tokenized_words_list = nltk.word_tokenize(self.samp_review)
+        stop_words_set = set(nltk.corpus.stopwords.words())
+    
+        for word in tokenized_words_list:
+            if word not in stop_words_set:
+                tokenized_without_stop_words_list.append(word)
 
-    def do_stemming(self):
-            pass
+        return tokenized_without_stop_words_list
+    
 
+    def do_stemming(self, tokenized_without_stop_words_list):
+        #Local ds
+        stemmed_word_list = [] 
+        stemmer = nltk.stem.PorterStemmer()
+   
+        print "Before stemming"
+        print tokenized_without_stop_words_list 
+
+        for word in tokenized_without_stop_words_list:
+            stemmed_word_list.append(stemmer.stem(word))
+
+
+        print stemmed_word_list
 
 if __name__ == "__main__":
     sn = sample_nltk()
