e017f4d504ff447009ee29039584561652679d22
Karthik Chandrasekar <karthikchandrasekar@Karthiks-MacBook-Pro.local>
2013-11-10 20:00:01 -0700

    Load movie review data

--- senti_analysis.py
+++ senti_analysis.py
@@ -1,42 +1,89 @@
-import nltk
+import nltk, os, logging
 
 class sample_nltk:
     def __init__(self):
-        self.samp_review = "No twist and turn. Just thala's presence made everything simple. BGM was classic. Aarambbam - a tribute to real heros, to and for thala fans. #bitch pls, it has just begun"        
 
+        #File names
+        self.bag_of_words_file_dir = "/Users/karthikchandrasekar/Desktop/Studies/Social_Media_Mining/SMM_PROJECT_2/INPUT/aclImdb"
+        self.bag_of_words_file_name = "imdb.vocab"
+
+        self.feat_file_dir = "/Users/karthikchandrasekar/Desktop/Studies/Social_Media_Mining/SMM_PROJECT_2/INPUT/aclImdb/train"
+        self.feat_file_name = "labeledBow.feat"
+
+        self.logger_file = os.path.join("OUTPUT", "senti_analysis.log")
+
+        #Global DS
+        self.id_to_word_dict = {}
+        self.id_to_bow_dict = {}
+        self.tokenized_without_stop_words_list = []
+        self.stemmed_word_list = []
+    
     def run_main(self):
-        tokenized_without_stop_words_list = self.do_tokenize()
-        self.do_stemming(tokenized_without_stop_words_list)
+        self.initialize_logger()
+        self.parse_reviews()
+        self.do_tokenize()
+        self.do_stemming()
+    
+    def initialize_logger(self):
+        logging.basicConfig(filename=self.logger_file, level=logging.INFO)
+        logging.info("Initialized logger")
 
+
+    def parse_reviews(self):
+        self.open_files()
+        self.load_data()
+        self.close_files()
+
+    def open_files(self):
+        self.bow = open(os.path.join(self.bag_of_words_file_dir, self.bag_of_words_file_name), 'r')
+        self.feat = open(os.path.join(self.feat_file_dir, self.feat_file_name), 'r')
+
+    def load_data(self):
+        self.load_bow()
+        self.load_feat()
+    
+
+    def load_bow(self):
+        uniq_id = 0
+        for line in self.bow.readlines():
+            if not line:    
+                continue
+            
+            self.id_to_word_dict[uniq_id] = line.strip()           
+            uniq_id += 1
+        logging.info("id_to_word_dict - length - %s" % (len(self.id_to_word_dict)))   
+
+ 
+    def load_feat(self):
+        uniq_id = 0
+        for line in self.feat.readlines():
+            if not line:
+                continue
+            self.id_to_bow_dict[uniq_id] =  line.strip().split(" ")  #First value in this line hold the labeled value
+            uniq_id += 1            
+        logging.info("id_to_bow_dict - length - %s" % (len(self.id_to_bow_dict)))
+
+    def close_files(self):
+        self.bow.close()
+        self.feat.close()
 
     def do_tokenize(self):
-
-        #Local ds
-        tokenized_without_stop_words_list = []
-
-        tokenized_words_list = nltk.word_tokenize(self.samp_review)
+        tokenized_words_list = nltk.word_tokenize("hello ")
         stop_words_set = set(nltk.corpus.stopwords.words())
     
         for word in tokenized_words_list:
             if word not in stop_words_set:
-                tokenized_without_stop_words_list.append(word)
+                self.tokenized_without_stop_words_list.append(word)
 
-        return tokenized_without_stop_words_list
+        return self.tokenized_without_stop_words_list
     
 
-    def do_stemming(self, tokenized_without_stop_words_list):
-        #Local ds
-        stemmed_word_list = [] 
+    def do_stemming(self):
         stemmer = nltk.stem.PorterStemmer()
-   
-        print "Before stemming"
-        print tokenized_without_stop_words_list 
 
-        for word in tokenized_without_stop_words_list:
-            stemmed_word_list.append(stemmer.stem(word))
+        for word in self.tokenized_without_stop_words_list:
+            self.stemmed_word_list.append(stemmer.stem(word))
 
-
-        print stemmed_word_list
 
 if __name__ == "__main__":
     sn = sample_nltk()
