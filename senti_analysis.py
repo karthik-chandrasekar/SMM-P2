b9a87859d5099bff1b9cdff884bbcd5ea1a422ac
Karthik Chandrasekar <karthidec1@gmail.com>
2013-11-28 18:15:28 -0700

    Adding feature extraction and classification - Development phase

--- senti_analysis.py
+++ senti_analysis.py
@@ -1,6 +1,6 @@
-import nltk, os, logging
-import json
-import ConfigParser
+import nltk, os, logging, json, ConfigParser, codecs
+import nltk.classify.util
+from nltk.classify import NaiveBayesClassifier
 
 class sample_nltk:
     def __init__(self):
@@ -8,12 +8,14 @@ class sample_nltk:
         self.config = ConfigParser.ConfigParser()
         self.config.read("senti_analysis.config")
 
-
         #File names
         self.bag_of_words_file_dir = self.config.get('GLOBAL','bag_of_words_file_dir')
         self.bag_of_words_file_name = self.config.get('GLOBAL', 'bag_of_words_file_name')
         self.feat_file_dir = self.config.get('GLOBAL', 'feat_file_dir')
         self.feat_file_name = self.config.get('GLOBAL', 'feat_file_name')
+        self.test_feat_file_dir = self.config.get('GLOBAL', 'test_feat_file_dir')
+        self.test_feat_file_name = self.config.get('GLOBAL', 'test_feat_file_name')
+    
 
         self.preprocessing_results_dir = self.config.get('PRE_PROCESSING', 'pre_processing_results_dir')
         self.preprocessing_results_file_name = self.config.get('PRE_PROCESSING', 'pre_processing_results_file_name')
@@ -27,12 +29,15 @@ class sample_nltk:
 
         self.tokenized_without_stop_words_list = []
         self.stemmed_word_list = []
-
+        self.reviews_list = []
+    
         self.stop_words_set = set()
     
     def run_main(self):
         self.preprocessing()
-
+        self.feature_selection()
+        self.feature_extraction()
+        self.classification()
 
     def preprocessing(self):
         self.initialize_logger()
@@ -40,6 +45,62 @@ class sample_nltk:
         self.remove_stop_words()
         self.do_stemming()
         self.dump_preprocessing()
+
+    def feature_selection(self):
+
+        self.pos_reviews, self.neg_reviews = self.get_labelled_reviews_words(self.reviews_list)
+        self.test_pos_reviews, self.test_neg_feats = self.get_labelled_reviews_words(self.feat_reviews_list)
+        
+        self.pos_tagged_words, self.neg_tagged_words = self.tag_words_with_labels(self.pos_reviews, self.neg_reviews) 
+        self.test_pos_tagged_words, self.test_neg_tagged_words = self.tag_words_with_labels(self.test_pos_reviews, self.test_neg_feats)    
+
+        self.train_feats = self.pos_tagged_reviews + self.neg_tagged_reviews
+        self.test_feats = self.test_post_tagged_words + self.test_neg_tagged_words  
+
+    def classification(self):
+        classifier = NaiveBayesClassifier.train(self.train_feats)
+        print 'accuracy:', nltk.classify.util.accuracy(classifier, self.test_feats)
+        classifier.show_most_informative_features() 
+
+
+    def get_labelled_reviews_words(self, reviews_list):
+        pos_reviews_words_list = neg_reviews_words_list = []
+
+        for review in reviews_list:
+            if not review:
+                continue
+
+            label = review[0]
+            if int(label) >= 7:
+                label = True
+            else if int(label) <=4:
+                label = False
+            else:
+                label = False
+
+            if label:
+                for word_freq_pair in review.split(" ")[1:]:
+                    if not word_freq_pair:
+                        continue
+                    word_id = str(word_freq_pair.split(":")[0])
+                    word = self.id_to_word_dict.get(word_id)
+                    words_list.append(word)
+                pos_reviews_words_list.append(words_list)
+            else: 
+                for word_freq_pair in review.split(" ")[1:]:
+                    if not word_freq_pair:
+                        continue
+                    word_id = str(word_freq_pair.split(":")[0])
+                    word = self.id_to_word_dict.get(word_id)
+                    words_list.append(word)
+                neg_reviews_words_list.append(word)
+            return (pos_reviews_words_list, neg_reviews_words_list)
+
+    def tag_words_with_labels(self, pos_reviews_words, neg_reviews_words):
+        pos_tagged_words = [(self.tag_words(word_list),'pos') for word_list in pos_reviews_words]
+        neg_tagged_words = [(self.tag_words(word_list),'neg') for word_list in neg_reviews_words]
+        return (pos_tagged_words, neg_tagged_words)
+        
  
     def initialize_logger(self):
         logging.basicConfig(filename=self.logger_file, level=logging.INFO)
@@ -52,12 +113,14 @@ class sample_nltk:
         self.close_files()
 
     def open_files(self):
-        self.bow = open(os.path.join(self.bag_of_words_file_dir, self.bag_of_words_file_name), 'r')
-        self.feat = open(os.path.join(self.feat_file_dir, self.feat_file_name), 'r')
+        self.bow = codecs.open(os.path.join(self.bag_of_words_file_dir, self.bag_of_words_file_name), 'r', 'utf-8')
+        self.feat = codecs.open(os.path.join(self.feat_file_dir, self.feat_file_name), 'r', 'utf-8')
+        self.test_feat = codecs.open(os.path.join(self.test_feat_file_dir, self.test_feat_file_name), 'r', 'utf-8')
 
     def load_data(self):
         self.load_bow()
-        self.load_feat()
+        self.reviews_list = self.load_feat(self.feat)
+        self.test_reviews_list = self.load_feat(self.test_feat)
         self.load_stop_words() 
 
     def load_bow(self):
@@ -71,14 +134,17 @@ class sample_nltk:
         logging.info("id_to_word_dict - length - %s" % (len(self.id_to_word_dict)))   
 
  
-    def load_feat(self):
+    def load_feat(self, fd):
         uniq_id = 0
-        for line in self.feat.readlines():
+        reviews_list = []
+
+        for line in fd.readlines():
             if not line:
                 continue
-            self.id_to_bow_dict[uniq_id] =  line.strip().split(" ")  #First value in this line hold the labeled value
+            reviews_list.append(line.strip())  #First value in this line hold the labeled value
             uniq_id += 1            
-        logging.info("id_to_bow_dict - length - %s" % (len(self.id_to_bow_dict)))
+        logging.info("total reviews  - length - %s" % (len(reviews_list)))
+        return reviews_list
 
     def load_stop_words(self):
         self.stop_words_set = set(nltk.corpus.stopwords.words())
@@ -86,6 +152,7 @@ class sample_nltk:
     def close_files(self):
         self.bow.close()
         self.feat.close()
+        self.test_feat.close()
 
     def remove_stop_words(self):
         for uniq_id, word in self.id_to_word_dict.iteritems():
@@ -98,7 +165,7 @@ class sample_nltk:
             self.id_to_word_dict[uniq_id] = stemmer.stem(word)
 
     def dump_preprocessing(self):
-        fd = open(self.preprocessing_results_file, 'w')
+        fd = codecs.open(self.preprocessing_results_file, 'w', 'utf-8')
         fd.write(json.dumps(self.id_to_word_dict))
         fd.close()
 
